6b57af26aa0ca8c1b479a981e830d8a512694b46
Karl Rito <karllouiserito08@gmail.com>
2026-07-09 14:46:15 +0800

    feat: rewrite header, footer, and layout shell with clean-red design

--- resources/js/LandingPages/indludes/footer.vue
+++ resources/js/LandingPages/indludes/footer.vue
@@ -1,144 +1,81 @@
 <template>
-    <div>
-        <section class="site-footer">
-            <div class="main-footer pt-142 pb-80">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-6 col-sm-12">
-                            <div class="footer-widget mb-40 footer-widget__about">
-                                <!-- <h3 class="text-white">Happy Kids Logo</h3> -->
-                                <img src="/images/happy_kids_logo.jpg" alt="happy_kids_logo" height="100" />
-                                <p>
-                                    <a href="https://trimexcolleges.edu.ph" target="_blank">Trimex Colleges</a><br />
-                                    Happy Kids
-                                </p>
-                                <ul class="list-unstyled footer-widget__contact">
-                                    <li>
-                                        <a href="tel:0495119278"><i class="azino-icon-telephone"></i>(049) 511-9278</a>
-                                    </li>
-                                    <li class="d-flex">
-                                        <a href="mailto:linkages@trimexcolleges.edu.ph"><i class="azino-icon-email"></i>linkages@trimexcolleges.edu.ph</a>
-                                    </li>
-                                    <li>
-                                        <a href="https://www.google.com/maps/place/Trimex+Colleges/@14.339063,121.0763387,16z/data=!4m10!1m2!2m1!1sTrimex+Colleges,+Bi%C3%B1an,+Laguna!3m6!1s0x3397d9fb7cc36f43:0x8b917e497bdafc50!8m2!3d14.339063!4d121.0853509!15sCh9UcmltZXggQ29sbGVnZXMsIEJpw7FhbiwgTGFndW5hkgEPcHJpdmF0ZV9jb2xsZWdl4AEA!16s%2Fg%2F11bwqqh8kv?entry=ttu" target="_blank"
-                                            ><i class="azino-icon-pin"></i>Trimex Colleges, Biñan, Laguna</a
-                                        >
-                                    </li>
-                                </ul>
-                                <!-- /.footer-widget__contact -->
-                            </div>
-                            <!-- /.footer-widget footer-widget__about -->
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-sm-12">
-                            <div class="footer-widget footer-widget__link mb-40">
-                                <h3 class="footer-widget__title">Explore</h3>
-                                <ul class="list-unstyled footer-widget__link-list">
-                                    <li><router-link :to="{ name: 'HomePage' }">Home</router-link></li>
-                                    <li><router-link :to="{ name: 'AboutPage' }">Our Mission</router-link></li>
-                                    <li><router-link :to="{ name: 'OurTeamPage' }">Our Team</router-link></li>
-                                    <li><router-link :to="{ name: 'FAQPage' }">FAQ</router-link></li>
-                                    <li><router-link :to="{ name: 'FeedingProgramPage' }">Feeding</router-link></li>
-                                    <li><router-link :to="{ name: 'ScholarshipProgramPage' }">Scholarship</router-link></li>
-                                    <li><router-link :to="{ name: 'EventsPage' }">Events</router-link></li>
-                                    <li><router-link :to="{ name: 'StoriesPage' }">Stories</router-link></li>
-                                    <li><router-link :to="{ name: 'ContactPage' }">Contact us</router-link></li>
-                                    <!-- <li><a href="contact.html">Terms</a></li> -->
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-sm-12">
-                            <div class="footer-widget mb-40 footer-widget__blog">
-                                <h3 class="footer-widget__title">Blog</h3>
-                                <ul class="list-unstyled footer-widget__blog">
-                                    <li class="pointer" @click="onRedirectBlog(a)" v-for="a in blogs" :key="a.id">
-                                        <!-- <img src="https://picsum.photos/68/70" alt="" /> -->
-                                        <img class="footer-thumb" :src="'/uploads/blogs/' + a?.original_blogs_id + '/thumb/' + a?.image" alt="" />
-                                        <p>{{ a.date | formatTransDate }}</p>
-                                        <h3>
-                                            <a href="javavascript:void(0);">{{ a.title }}</a>
-                                        </h3>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="footer-widget mb-40 footer-widget__newsletter">
-                                <h3 class="footer-widget__title">Newletter</h3>
-                                <p>Signup now to get daily latest news & updates from us</p>
-                                <form class="footer-widget__newsletter-form mc-form">
-                                    <label for="mc-email" class="sr-only">Email Address</label>
-                                    <input type="email" name="EMAIL" id="mc-email" class="dynamic-radius px-3" placeholder="Email address" />
-                                    <div class="footer-widget__newsletter-btn-wrap d-flex justify-content-center">
-                                        <button type="submit" class="btn btn-md btn-secondary text-warning">Subscribe Now</button>
-                                    </div>
-                                </form>
-                                <div class="mc-form__response"></div>
-                            </div>
-                        </div> -->
-                        <!-- /.col-lg-3 col-md-6 col-sm-12 -->
-                    </div>
-                    <!-- /.row -->
-                </div>
-                <!-- /.container -->
+  <footer class="hk-footer">
+    <div class="hk-container">
+      <div class="hk-footer-grid">
+        <div>
+          <img src="/images/happy_kids_logo.jpg" alt="Happy Kids" />
+          <p>
+            <a href="https://trimexcolleges.edu.ph" target="_blank">Trimex Colleges</a><br />
+            Happy Kids
+          </p>
+          <ul class="hk-footer-contact">
+            <li><i class="fas fa-phone"></i> <a href="tel:0495119278">(049) 511-9278</a></li>
+            <li><i class="fas fa-envelope"></i> <a href="mailto:linkages@trimexcolleges.edu.ph">linkages@trimexcolleges.edu.ph</a></li>
+            <li><i class="fas fa-map-marker-alt"></i> <a href="https://www.google.com/maps/place/Trimex+Colleges/@14.339063,121.0763387,16z/data=!4m10!1m2!2m1!1sTrimex+Colleges,+Bi%C3%B1an,+Laguna!3m6!1s0x3397d9fb7cc36f43:0x8b917e497bdafc50!8m2!3d14.339063!4d121.0853509!15sCh9UcmltZXggQ29sbGVnZXMsIEJpw7FhbiwgTGFndW5hkgEPcHJpdmF0ZV9jb2xsZWdl4AEA!16s%2Fg%2F11bwqqh8kv?entry=ttu" target="_blank">Trimex Colleges, Biñan, Laguna</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>Explore</h4>
+          <ul class="hk-footer-links">
+            <li><router-link :to="{ name: 'HomePage' }">Home</router-link></li>
+            <li><router-link :to="{ name: 'AboutPage' }">Our Mission</router-link></li>
+            <li><router-link :to="{ name: 'OurTeamPage' }">Our Team</router-link></li>
+            <li><router-link :to="{ name: 'FAQPage' }">FAQs</router-link></li>
+            <li><router-link :to="{ name: 'FeedingProgramPage' }">Feeding Program</router-link></li>
+            <li><router-link :to="{ name: 'ScholarshipProgramPage' }">Scholarship Program</router-link></li>
+            <li><router-link :to="{ name: 'EventsPage' }">Events</router-link></li>
+            <li><router-link :to="{ name: 'StoriesPage' }">Stories</router-link></li>
+            <li><router-link :to="{ name: 'ContactPage' }">Contact Us</router-link></li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>Latest Stories</h4>
+          <div class="hk-sidebar-post" v-for="blog in blogs" :key="blog.id" @click="onRedirectBlog(blog)">
+            <img :src="'/uploads/blogs/' + blog?.original_blogs_id + '/thumb/' + blog?.image" alt="" />
+            <div>
+              <h5>{{ blog.title }}</h5>
+              <span>{{ blog.date | formatTransDate }}</span>
             </div>
-            <!-- /.main-footer -->
-            <div class="footer-bottom">
-                <div class="container">
-                    <span class="scroll-to-top scroll-to-target" @click="scrollToTop"><i class="fas fa-angle-up"></i></span>
-                    <!-- <p>© Copyright 2023 by Layerdrops.com</p> -->
-                    <p>© Copyright 2023</p>
-                    <div class="footer-social">
-                        <!-- <a href="#" aria-label="twitter"><i class="fab fa-twitter"></i></a> -->
-                        <a target="_blank" href="https://www.facebook.com/TrimexCollegesHappyKids/" aria-label="facebook"><i class="fab fa-facebook-square"></i></a>
-                        <!-- <a href="#" aria-label="pinterest"><i class="fab fa-pinterest-p"></i></a> -->
-                        <a target="_blank" href="https://www.instagram.com/trimexcollegeshappykids/" aria-label="instagram"><i class="fab fa-instagram"></i></a>
-                    </div>
-                    <!-- /.footer-social -->
-                </div>
-                <!-- /.container -->
-            </div>
-            <!-- /.footer-bottom -->
-        </section>
-        <!-- /.site-footer -->
+          </div>
+        </div>
+      </div>
+
+      <div class="hk-footer-bottom">
+        <p>© Copyright {{ new Date().getFullYear() }} Happy Kids. All rights reserved.</p>
+        <div class="hk-footer-social">
+          <a target="_blank" href="https://www.facebook.com/TrimexCollegesHappyKids/" aria-label="facebook"><i class="fab fa-facebook-square"></i></a>
+          <a target="_blank" href="https://www.instagram.com/trimexcollegeshappykids/" aria-label="instagram"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
     </div>
+  </footer>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            blogs: [],
-        };
+  data() {
+    return { blogs: [] };
+  },
+  methods: {
+    onPopulateData() {
+      this.$front_queries("blogs", {
+        action_type: "take_2_blogs",
+      })
+        .then((res) => {
+          this.blogs = res.data.data.blogs;
+        })
+        .catch((err) => {
+          console.error("footer error:" + err);
+        });
     },
-
-    methods: {
-        scrollToTop() {
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-            });
-        },
-
-        onPopulateData() {
-            this.$front_queries("blogs", {
-                action_type: "take_2_blogs",
-            })
-                .then((res) => {
-                    let response = res.data.data.blogs;
-                    this.blogs = response;
-                })
-                .catch((err) => {
-                    console.error("error footer:" + err);
-                });
-        },
-
-        onRedirectBlog(data) {
-            this.$router.push({ name: "StoriesDetailsPage", params: { slug: data.slug } }).catch(() => {});
-        },
+    onRedirectBlog(data) {
+      this.$router.push({ name: "StoriesDetailsPage", params: { slug: data.slug } }).catch(() => {});
     },
-
-    created() {
-        this.onPopulateData();
-    },
+  },
+  created() {
+    this.onPopulateData();
+  },
 };
 </script>
--- resources/js/LandingPages/indludes/header.vue
+++ resources/js/LandingPages/indludes/header.vue
@@ -1,164 +1,76 @@
 <template>
-  <div>
-    <div class="main-header__two">
-      <div class="main-header__top">
-        <div class="container">
-          <p>Welcome to non profit charity platform</p>
-
-          <div class="main-header__social">
-            <!-- <a href="#" aria-label="twitter"><i class="fab fa-twitter"></i></a> -->
-            <a target="_blank" href="https://www.facebook.com/TrimexCollegesHappyKids/" aria-label="facebook"><i class="fab fa-facebook-square"></i></a>
-            <!-- <a href="#" aria-label="pinterest"><i class="fab fa-pinterest-p"></i></a> -->
-            <a target="_blank" href="https://www.instagram.com/trimexcollegeshappykids/" aria-label="instagram"><i class="fab fa-instagram"></i></a>
+  <header class="hk-header" :class="{ 'is-sticky': isSticky }" ref="header">
+    <div class="hk-container">
+      <div class="hk-header-inner">
+        <router-link :to="{ name: 'HomePage' }" class="hk-logo">
+          <img src="/images/happy_kids_logo.jpg" alt="Happy Kids" />
+          <div>
+            <h4 style="margin:0;font-size:1rem;">Trimex Colleges</h4>
+            <span style="font-size:0.75rem;color:#6e6e73;">Happy Kids</span>
           </div>
-          <!-- /.main-header__social -->
-        </div>
-        <!-- /.container -->
-      </div>
-      <!-- /.main-header__top -->
-      <div class="header-upper">
-        <div class="container">
-          <div class="logo-box d-flex align-items-center">
-            <img src="/images/happy_kids_logo.jpg" alt="happy_kids_logo" height="60" />
-            <span class="fa fa-bars mobile-nav__toggler"></span>
-            <div class="ml-2">
-              <a target="_blank" href="https://trimexcolleges.edu.ph/"><h4 class="fw-bold">Trimex Colleges</h4></a>
-              <h6 class="fw-bold">Happy Kids</h6>
+        </router-link>
+
+        <nav class="hk-nav">
+          <router-link :to="{ name: 'HomePage' }">Home</router-link>
+          <div class="hk-dropdown">
+            <a href="javascript:void(0)" class="hk-nav-link">About</a>
+            <div class="hk-dropdown-menu">
+              <router-link :to="{ name: 'AboutPage' }">Our Mission</router-link>
+              <router-link :to="{ name: 'OurTeamPage' }">Our Team</router-link>
+              <router-link :to="{ name: 'FAQPage' }">FAQs</router-link>
             </div>
           </div>
-          <!-- /.logo-box -->
-
-          <div class="header-info">
-            <div class="header-info__box">
-              <i class="azino-icon-email1"></i>
-              <div class="header-info__box-content">
-                <h3>Email</h3>
-                <p><a href="mailto:linkages@trimexcolleges.edu.ph">linkages@trimexcolleges.edu.ph</a></p>
-              </div>
-              <!-- /.header-info__box-content -->
+          <div class="hk-dropdown">
+            <a href="javascript:void(0)" class="hk-nav-link">Our Program</a>
+            <div class="hk-dropdown-menu">
+              <router-link :to="{ name: 'FeedingProgramPage' }">Feeding</router-link>
+              <router-link :to="{ name: 'ScholarshipProgramPage' }">Scholarship</router-link>
+              <router-link :to="{ name: 'EventsPage' }">Events</router-link>
             </div>
-            <!-- /.header-info__box -->
-            <div class="header-info__box">
-              <i class="azino-icon-calling"></i>
-              <div class="header-info__box-content">
-                <h3>Phone</h3>
-                <p><a href="tel:0495119278">(049) 511-9278</a></p>
-              </div>
-              <!-- /.header-info__box-content -->
-            </div>
-            <!-- /.header-info__box -->
-            <div class="header-info__box">
-              <i class="azino-icon-address"></i>
-              <div class="header-info__box-content">
-                <h3>Visit</h3>
-                <p>
-                  <a href="https://www.google.com/maps/place/Trimex+Colleges/@14.339063,121.0763387,16z/data=!4m10!1m2!2m1!1sTrimex+Colleges,+Bi%C3%B1an,+Laguna!3m6!1s0x3397d9fb7cc36f43:0x8b917e497bdafc50!8m2!3d14.339063!4d121.0853509!15sCh9UcmltZXggQ29sbGVnZXMsIEJpw7FhbiwgTGFndW5hkgEPcHJpdmF0ZV9jb2xsZWdl4AEA!16s%2Fg%2F11bwqqh8kv?entry=ttu" target="_blank"
-                    >Trimex Colleges, <br />
-                    Biñan, Laguna</a
-                  >
-                </p>
-              </div>
-              <!-- /.header-info__box-content -->
-            </div>
-            <!-- /.header-info__box -->
           </div>
-          <!-- /.header-info -->
-        </div>
-        <!-- /.container -->
-      </div>
-      <!-- /.header-upper -->
-      <nav class="main-menu">
-        <div class="container bg-transparent">
-          <ul class="main-menu__list dynamic-radius d-flex align-items-center justify-content-around">
-            <li><router-link :to="{ name: 'HomePage' }">Home</router-link></li>
-            <li class="dropdown">
-              <a href="javascript:void(0)">About</a>
-              <ul>
-                <li><router-link :to="{ name: 'AboutPage' }">Our Mission</router-link></li>
-                <li><router-link :to="{ name: 'OurTeamPage' }">Our Team</router-link></li>
-                <li><router-link :to="{ name: 'FAQPage' }">FAQs</router-link></li>
-              </ul>
-            </li>
-            <li class="dropdown">
-              <a href="javascript:void(0)">Our Program</a>
-              <ul>
-                <li><router-link :to="{ name: 'FeedingProgramPage' }">Feeding </router-link></li>
-                <li><router-link :to="{ name: 'ScholarshipProgramPage' }">Scholarship </router-link></li>
-                <li><router-link :to="{ name: 'EventsPage' }">Events</router-link></li>
-              </ul>
-            </li>
-            <li><router-link :to="{ name: 'StoriesPage' }">Stories</router-link></li>
-            <li><router-link :to="{ name: 'ContactPage' }">Contact Us</router-link></li>
-          </ul>
-          <!-- /.main-menu__list -->
-          <a @click="onDonate" href="javascript:void(0)" class="thm-btn dynamic-radius">Donate Now</a>
-          <!-- /.thm-btn dynamic-radius -->
-        </div>
-        <!-- /.container -->
-      </nav>
-      <!-- /.main-menu -->
-    </div>
-    <!-- /.main-header__two -->
+          <router-link :to="{ name: 'StoriesPage' }">Stories</router-link>
+          <router-link :to="{ name: 'ContactPage' }">Contact Us</router-link>
+        </nav>
 
-    <div class="stricky-header stricked-menu stricky-header-two">
-      <div class="container">
-        <div class="logo-box">
-          <a href="javaScript:void(0);"><img src="/images/happy_kids_logo.jpg" width="50" alt="happy_kids_logo" /></a>
-        </div>
-        <!-- /.logo-box -->
-        <div class="sticky-header__content">
-          <ul class="main-menu__list dynamic-radius">
-            <li><router-link :to="{ name: 'HomePage' }">Home</router-link></li>
-            <li class="dropdown">
-              <a href="javascript:void(0)">About</a>
-              <ul>
-                <li><router-link :to="{ name: 'AboutPage' }">Our Mission</router-link></li>
-                <li><router-link :to="{ name: 'OurTeamPage' }">Our Team</router-link></li>
-                <li><router-link :to="{ name: 'FAQPage' }">FAQs</router-link></li>
-              </ul>
-            </li>
-            <li class="dropdown">
-              <a href="javascript:void(0)">Our Program</a>
-              <ul>
-                <li><router-link :to="{ name: 'FeedingProgramPage' }">Feeding Program</router-link></li>
-                <li><router-link :to="{ name: 'ScholarshipProgramPage' }">Scholarship Program</router-link></li>
-                <li><router-link :to="{ name: 'EventsPage' }">Events</router-link></li>
-              </ul>
-            </li>
-            <li><router-link :to="{ name: 'StoriesPage' }">Stories</router-link></li>
-            <li><router-link :to="{ name: 'ContactPage' }">Contact Us</router-link></li>
-            
-        </ul>
-        <!-- /.main-menu__list -->
-    </div>
-    <!-- /.sticky-header__content -->
-    <button @click="onDonate" href="javascript:void(0)" class="btn btn-danger">Donate Now</button>
+        <button class="hk-btn hk-btn-gold hk-btn-sm" @click="onDonate">Donate Now</button>
+
+        <button class="hk-mobile-toggle" @click="mobileOpen = !mobileOpen">☰</button>
       </div>
-      <!-- /.container -->
     </div>
-    <!-- /.stricky-header -->
+
+    <div class="hk-mobile-nav" :class="{ open: mobileOpen }">
+      <router-link @click.native="mobileOpen = false" :to="{ name: 'HomePage' }">Home</router-link>
+      <router-link @click.native="mobileOpen = false" :to="{ name: 'AboutPage' }">Our Mission</router-link>
+      <router-link @click.native="mobileOpen = false" :to="{ name: 'OurTeamPage' }">Our Team</router-link>
+      <router-link @click.native="mobileOpen = false" :to="{ name: 'FAQPage' }">FAQs</router-link>
+      <router-link @click.native="mobileOpen = false" :to="{ name: 'FeedingProgramPage' }">Feeding Program</router-link>
+      <router-link @click.native="mobileOpen = false" :to="{ name: 'ScholarshipProgramPage' }">Scholarship Program</router-link>
+      <router-link @click.native="mobileOpen = false" :to="{ name: 'EventsPage' }">Events</router-link>
+      <router-link @click.native="mobileOpen = false" :to="{ name: 'StoriesPage' }">Stories</router-link>
+      <router-link @click.native="mobileOpen = false" :to="{ name: 'ContactPage' }">Contact Us</router-link>
+    </div>
+
     <donate-modal @onHideModal="onCancelForm" @success="onSuccess" />
-  </div>
+  </header>
 </template>
 
 <script>
 import DonateModal from "./donate_modal.vue";
 export default {
   components: { DonateModal },
-
+  data() { return { isSticky: false, mobileOpen: false }; },
+  mounted() {
+    window.addEventListener('scroll', () => {
+      this.isSticky = window.scrollY > 10;
+    });
+  },
   methods: {
     onDonate() {
       $("#donate_modal").modal("show");
       $("#donate_modal").appendTo("body");
     },
-
-    onCancelForm() {
-      $("#donate_modal").modal("hide");
-    },
-
-    onSuccess() {
-      $("#donate_modal").modal("hide");
-    },
+    onCancelForm() { $("#donate_modal").modal("hide"); },
+    onSuccess() { $("#donate_modal").modal("hide"); },
   },
 };
 </script>
